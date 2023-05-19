328f25d91961717ae037680a434b0ad884dfc622
web3ten0 <web3ten0@gmail.com>
2023-05-19 20:49:59 +0900

    added editor.status

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,20 @@
 	import { commonmark } from '@milkdown/preset-commonmark';
 	import { nord } from '@milkdown/theme-nord';
 
-	const editor = (dom: any) => {
-		Editor.make()
+	const editor = Editor.make().config(nord).use(commonmark);
+
+	console.log('status1', editor.status);
+
+	const createEditor = (dom: any) => {
+		editor
 			.config((ctx) => {
 				ctx.set(rootCtx, dom);
 			})
-			.config(nord)
-			.use(commonmark)
-			.create();
+			.create()
+			.then(() => {
+				console.log('status2', editor.status);
+			});
 	};
 </script>
 
-<div use:editor />
+<div use:createEditor />
